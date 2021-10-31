0ea8440fe26048e39506d1ff09a2531f9158390d
hll-ai <halil.seran.1135@gmail.com>
2021-10-31 13:22:33 +0300

    deneme

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import FirstComponent from './firstComponent';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    //Vievin içinde harici bir stil tanımlamak için
+    <View style={{flexDirection:'column',flex:1}}>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      <View style={{flex:1,flexDirection:'row-reverse'}}>      
+        <View style={{width:50,height:50,backgroundColor:'green'}}    />
+        <View style={{width:50,height:50,backgroundColor:'red'}}      />
+        <View style={{width:50,height:50,backgroundColor:'yellow'}}   />
+        <View style={{width:50,height:50,backgroundColor:'blue'}}     />        
+      </View>
+        
+      <View style={{flex:1,flexDirection:'row',justifyContent:'space-around'}}>
+        <View style={{width:50,height:50,backgroundColor:'green'}}    />
+        <View style={{width:50,height:50,backgroundColor:'red'}}      />
+        <View style={{width:50,height:50,backgroundColor:'yellow'}}   />
+        <View style={{width:50,height:50,backgroundColor:'blue'}}     />        
+      </View>
+
+      <View style={{flex:1,flexDirection:'row',justifyContent:'center'}}>
+        <View style={{width:50,height:50,backgroundColor:'green'}}    />
+        <View style={{width:50,height:50,backgroundColor:'red'}}      />
+        <View style={{width:50,height:50,backgroundColor:'yellow'}}   />
+        <View style={{width:50,height:50,backgroundColor:'blue'}}     />        
+      </View>
+
+      <View style={{flex:1,flexWrap:'wrap',flexDirection:'row',justifyContent:'space-around'}}>
+        <View style={{width:150,height:50,backgroundColor:'green'}}    />
+        <View style={{width:150,height:50,backgroundColor:'red'}}      />
+        <View style={{width:150,height:50,backgroundColor:'yellow'}}   />
+        <View style={{width:50,height:50,backgroundColor:'blue'}}     />        
+      </View>
+
+      <View style={{flex:1,flexDirection:'row',alignItems:'flex-end'}}>
+        <View style={{width:50,height:50,backgroundColor:'green'}}    />
+        <View style={{width:50,height:50,backgroundColor:'red'}}      />
+        <View style={{width:50,height:50,backgroundColor:'yellow'}}   />
+        <View style={{width:50,height:50,backgroundColor:'blue'}}     />        
+      </View>
+
+    </View>   
+
+  );
+};
+
+//justify content
+//flex flexDirection
+//flex wrap: wrap şeklin devamını aşağıdan devam ettirir 
+//alignitem flexin içinde nereden başlayacağını belirler
+
+
+const style = StyleSheet.create({
+  giris:{paddingTop:150,
+  backgroundColor:'red',
+  flex:1
   },
+  giris_yazı:{
+    color:'white',
+    fontSize:30
+  }
+  
 });
+
+
+
+//böyle de yazılır ama boş iş
+/* 
+const App = () => {
+  return(
+    <View style={{paddingTop:200}}>
+      <Text>Merhaba</Text>
+    </View>
+  )
+}
+export default App;
+*/
---
+++ firstComponent.js
@@ -0,0 +1,23 @@
+import React,{Component} from "react";
+import {View, Text} from "react-native";
+
+
+/*
+const FirstComponent = () => {
+    return(
+        <View>
+            <Text>first component</Text>
+        </View>
+    )
+}
+export default FirstComponent;
+*/
+
+export default class FirstComponent extends React.Component {
+    render(){
+    return(
+        <View>
+            <Text>first component</Text>
+        </View>
+    )
+}}
